5474c694e4668bb310fea2b1e43b85f2e6d75036
WangNing-Zhou <1879154660@qq.com>
2023-02-14 04:58:24 +0800

    后端 初始化路由,将路由添加激活

--- backend/src/init/initRoute.js
+++ backend/src/init/initRoute.js
@@ -1,16 +1,20 @@
-const userRoute = require('../routes/user')
-const followerRouter = require('../routes/follow');
-const articlesRouter = require('../routes/articles');
-
-const initRoute = (app)=>{
-    app.get('/',(req,res)=>{
-        res.json({status:'API is running'});
+const userRouter = require("../routes/users");
+const followRouter = require("../routes/follow");
+const favoriteRouter = require("../routes/favorites");
+const tagRouter = require("../routes/tags");
+const commentRouter = require("../routes/comments");
+const articleRouter = require("../routes/articles");
+const initRoute = (app)=> {
+    app.get('/', (req, res) => {
+        res.json({status: 'API is running'});
     })
 
     //使用路由
-    app.use('/api/v1/users',userRoute);
-    app.use('/api/v1/follow',followerRouter);
-    app.use('/api/v1/articles',articlesRouter)
-}
-
+    app.use("/api/v1/users", userRouter);
+    app.use("/api/v1/follow", followRouter);
+    app.use("/api/v1/favorites", favoriteRouter);
+    app.use("/api/v1/tags", tagRouter);
+    app.use("/api/v1/comments", commentRouter);
+    app.use("/api/v1/articles", articleRouter);
+};
 module.exports = initRoute;
