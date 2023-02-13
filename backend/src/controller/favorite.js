f6634a6b9e34c379e83c36a5593e246104cf131a
WangNing-Zhou <1879154660@qq.com>
2023-02-14 04:33:36 +0800

    后端 取消喜欢文章

--- backend/src/controller/favorite.js
+++ backend/src/controller/favorite.js
@@ -2,7 +2,14 @@ const HttpException = require('../models/User')
 const {User,Article,Tag} = require('../models')
 const {handleArticle} = require('../utils/common')
 
-//添加喜欢的文章
+/**
+ * 添加喜欢的文章: 当前登录用户
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<void>}
+ */
+
 const addFavorite =async (req,res,next)=>{
     try {
         const {slug} = req.params;
@@ -18,7 +25,7 @@ const addFavorite =async (req,res,next)=>{
         //喜欢的人数
         const count = await article.countUsers();
 
-        const data =handleArticle(article,article.user,count+1,true);
+        const data =handleArticle(article,article.user,count,true);
 
         res.status(200).json({
             status:1,
@@ -28,6 +35,47 @@ const addFavorite =async (req,res,next)=>{
     }catch (err){
         next(err)
     }
+}
+
+/**
+ * 取消喜欢文章: 当前登录用户
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<void>}
+ */
+
+const  removeFavorite = async (req,res,next)=>{
+    try{
+        const {slug} = req.params;
+
+        const article = await Article.findByPk(slug,{include:[Tag,User]});
+        if(!article){
+            throw new HttpException(404,"文章不存在","article not found");
+        }
+
+        //取消当前登录用户对这篇文章的喜欢
+        await article.removeUser(req.user.email);
+
+        //喜欢的人数
+        const count = await article.countUser();
+
+        //生成文章返回数据
+        const data = handleArticle(article,article.user,count,false);
+        res.status(200).json({
+            status: 1,
+            message: '取消成功',
+            data
+        });
+
+    }catch (err){
+        next(err)
+    }
+}
+
+module.exports = {
+    addFavorite,
+    removeFavorite
 }
 
 
