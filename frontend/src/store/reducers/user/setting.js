0e33c4eeab2231528f723b92c151e8b9deba1ab3
WangNing-Zhou <1879154660@qq.com>
2023-02-16 06:31:08 +0800

    前端 设置reducer实现

---
+++ frontend/src/store/reducers/user/setting.js
@@ -0,0 +1,63 @@
+import * as constant from "../../../constant"
+import {saveDate, getDate, deleteDate} from "../../../utils/localStorage"
+
+//初始化用户
+const initUser = () => {
+    const currentUser = getDate("currentUser")
+    if (currentUser) {
+        return currentUser
+    }
+    return null
+}
+
+//初始化token,用户更新操作需要验证用户身份
+const initToken = () => {
+    const token = getDate("token");
+    if ("token") {
+        return token
+    }
+    return null
+}
+
+const initState = {
+    ...initUser(),
+    errors: null,
+    currentUser: initUser(),
+    token: initToken()
+}
+
+const settingReducer = (state = initState, action) => {
+    switch (action.type) {
+        case constant.SETTING_FIELD:
+            const key = action.key
+            const value = action.valueOf
+            return {...state, [key]: value};
+        case constant.SETTING_UNLOAD:
+            return {...initState, currentUser: initUser(), token: initToken()}
+        case constant.SETTING_RESULT:
+            const {status, message, data} = action.result
+            if (status === 1) {
+                let currentUser = data
+                let token = data.token
+                saveDate('currentUser', currentUser);
+                saveDate('token', token);
+                return {...state, errors: message}
+            } else {
+                return {...state, errors: message}
+            }
+        case constant.SETTING_LOGOUT:
+            state = {}
+            deleteDate("currentUser");
+            deleteDate("token");
+            return {...state, redirect: '/login'}
+        default:
+            return {
+                ...initUser(),
+                errors: null,
+                currentUser: initUser(),
+                token: initToken()
+            }
+    }
+}
+
+export default  settingReducer;
